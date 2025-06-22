9971cfe8e2fe2c43e22e69b939a972d1cd20d5c0
Oktay <oktay@yatko.dev>
2025-06-22 23:49:21 +0200

    forgot await

--- app/_dyn.tsx
+++ app/_dyn.tsx
@@ -5,7 +5,7 @@ export const experimental_ppr = true;
 
 export default async function Dyn() {
   await connection();
-  const value = calc();
+  const value = await calc();
   return (
     <article>
       <h1>Dyn component</h1>
